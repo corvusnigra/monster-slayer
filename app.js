d3971ed97eb323195d09dd122ed6a20e3a55f7c5
ymarkov <itemruby@gmail.com>
2017-10-30 17:46:51 +0300

    add app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#app',
+
+    data: {
+        playerHealth: 100,
+        monsterHealth: 100,
+        isGameRunning: false
+
+    },
+
+    methods: {
+        startGame: function(){
+            this.isGameRunning = true;
+            this.monsterHealth = 100;
+            this.playerHealth = 100;
+        }
+    }
+
+
+});
